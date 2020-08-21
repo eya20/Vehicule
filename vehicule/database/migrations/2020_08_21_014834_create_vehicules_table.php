2f088f6034027fea31eed25e61dd9489c8edfd0f
Eya <bentali.aya@gmail.com>
2020-08-21 03:27:01 +0100

    tables migrations

---
+++ vehicule/database/migrations/2020_08_21_014834_create_vehicules_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehiculesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicules', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->integer('marque');
+            $table->integer('modele');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicules');
+    }
+}
